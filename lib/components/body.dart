c31a816d35b589f7d476c22eaeb1cf5acc9a0d6e
hungnv <hungnv@mor.com.vn>
2022-10-11 14:01:07 +0700

    fix

--- lib/components/body.dart
+++ lib/components/body.dart
@@ -19,8 +19,43 @@ class Body extends StatelessWidget {
             title: "Recommend",
             press: () {},
           ),
-          RecommendPlants()
+          const RecommendPlants(),
+          TitleWithMoreBtn(title: "Featured Plants", press: () {}),
+          // Row(
+          //   children: [
+          //     FeaturePlantCard(image: "assets/images/bottom_img_1.png"),
+          //   ],
+          // ),
         ],
+      ),
+    );
+  }
+}
+
+class FeaturePlantCard extends StatelessWidget {
+  const FeaturePlantCard({
+    Key? key,
+    required this.image, required this.press,
+  }) : super(key: key);
+
+  final String image;
+  final Function press;
+
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    return GestureDetector(
+      onTap: () => press,
+      child: Container(
+        margin: const EdgeInsets.only(
+            left: kDefaultPadding,
+            top: kDefaultPadding / 2,
+            right: kDefaultPadding / 2),
+        width: size.width * 0.8,
+        height: 185,
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(10),
+            image: DecorationImage(image: AssetImage(image))),
       ),
     );
   }
